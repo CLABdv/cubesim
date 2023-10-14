77cff8f255fc0fd07d0f2f65fdffe4055f7a7380
Casper Beckman <casper@debstation.home>
2023-10-14 15:11:08 +0200

    added some comments

--- app/CubeData.hs
+++ app/CubeData.hs
@@ -3,9 +3,12 @@
 module CubeData where
 import Foreign.Ptr
 import Foreign.C
---https://wiki.haskell.org/CPlusPlus_from_Haskell 
 
 data Cubepos = Cubepos
+-- I don't really know what I should put as return type,
+-- IO () doesnt really feel correct since the functions don't really do IO, it just mutates the Ptr Cubepos
+-- I might write the wrapper such that it returns the pointer each time, that will also force the evaluation 
+-- to happen in the correct order. This will probably be needed.
 foreign import capi "cwrapper_cubepos.h c_cubepos_init" cubeposInit :: CInt -> CInt -> CInt -> Ptr Cubepos
 foreign import capi "cwrapper_cubepos.h c_cubepos_delete" cubeposDelete :: Ptr Cubepos -> IO ()
 foreign import capi "cwrapper_cubepos.h c_parse_move" parseMove :: Ptr Cubepos -> Ptr CChar -> CInt
